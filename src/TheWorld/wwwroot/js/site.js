32f4ebbaf61176185e64884512410d8d6fecd1cb
alim727 <alim727@gmail.com>
2016-01-28 23:50:50 -0500

    bower, jquery, mvc set-up

--- src/TheWorld/wwwroot/js/site.js
+++ src/TheWorld/wwwroot/js/site.js
@@ -1,4 +1,34 @@
 ﻿// site.js
 
-var ele = document.getElementById("username");
-ele.innerHTML = "Ali Mourad";
+(function () {
+//    $
+
+//    var ele = $("#username");
+//    ele.text("Ali Mourad");
+
+//    var main = $("#main");
+//    main.on("mouseenter", function () {
+//        main.style = "background-color: #888";
+//    });
+//    main.on("mouseleave", function () {
+//        main.style["background-color"] = "";
+//    });
+
+//    var menuItems = $("ul.menu li a");
+//    menuItems.on("click", function () {
+//        var me = $(this);
+//    alert(me.text());
+    //});
+
+    var $sidebarAndWrapper = $("#sidebar,#wrapper");
+
+    // logic to implement hide-sidebar class and then logic to change toggle button text
+    $("#sidebarToggle").on("click", function () {
+        $sidebarAndWrapper.toggleClass("hide-sidebar");
+        if ($sidebarAndWrapper.hasClass("hide-sidebar")) {
+            $(this).text("Show Sidebar");
+        } else {
+            $(this).text("Hide Sidebar");
+        }
+    });
+}) ();
